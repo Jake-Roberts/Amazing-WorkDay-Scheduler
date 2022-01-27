719da083f43826e8d8a2cd029ba5970d710aac44
JakeRoberts99 <jake.s.roberts99@gmail.com>
2022-01-26 19:58:18 -0700

    added the ability to save to local storage, also gave the textarea a border so you can see where to input data

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
-// add current day function or var 
+
+// add current day function or var
 $("#currentDay").text(moment().format("MMM Do YYYY"));
 // add a save to local storage function 
+$(".saveBtn").on("click", function () {
+    
+    var textInput = $(this).siblings(".input-area").val();
+    var hours = $(this).parent().attr("id");
+    console.log(hours)
+    console.log(this)
+    //save to local
+    localStorage.setItem(hours, textInput)
+})
+
+
+
+
+
 
 // add color's for past present and future 
